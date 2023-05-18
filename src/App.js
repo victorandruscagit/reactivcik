c8ee87e3ab6d7aa19204ddcf7533d9dee9f9420d
victorandruscagit <victor.v.andrusca@gmail.com>
2023-05-18 13:04:15 +0300

    fisr

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     
+        <p>
+          We made our first React application
+        </p>
+     
+    </div>
+  );
+}
+
+export default App;
