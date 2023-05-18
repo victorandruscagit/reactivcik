a48b5c04eca2c51daf7b290d0651fae89c085ff0
victorandruscagit <victor.v.andrusca@gmail.com>
2023-05-18 14:11:31 +0300

    todolist table

--- src/App.js
+++ src/App.js
@@ -3,11 +3,37 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-     
-        <p>
-          We made our first React application
-        </p>
+    <div >
+     <div>
+      <div>
+        Your Todo's.
+      </div>
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <th scope='col'>#</th>
+              <th scope='col'>Description</th>
+              <th scope='col'>Assinged</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope='row'>1</th>
+              <td>Feed dog</td>
+              <td>Eric</td>
+            </tr>
+            <tr>
+              <th scope='row'>2</th>
+              <td>Get haircut</td>
+              <td>Eric</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      
+     </div>
+       
      
     </div>
   );
