0185362185ba5854f3a390b358098e80c9766577
victorandruscagit <victor.v.andrusca@gmail.com>
2023-05-19 11:00:27 +0300

    added first child component in src/components and exported it

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import TodoRowItem from './components/TodoRowItem';
 
 function App() {
   return (
@@ -18,11 +19,7 @@ function App() {
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <th scope='row'>1</th>
-              <td>Feed dog</td>
-              <td>Eric</td>
-            </tr>
+            <TodoRowItem/>
             <tr>
               <th scope='row'>2</th>
               <td>Get haircut</td>
---
+++ src/components/TodoRowItem.js
@@ -0,0 +1,15 @@
+function TodoRowItem(){
+    const rowNumber =1;
+    const rowDescription = 'Feed Dog';
+    const rowAssigned = 'Eric';
+
+    return(
+        <tr>
+            <th scope ="row" >{rowNumber}</th>
+            <td>{rowDescription}</td>
+            <td>{rowAssigned}</td>
+        </tr>
+    )
+}
+
+export default TodoRowItem
